4f753762b38b89bbcc56b4a463876230667430a1
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2025-07-17 20:56:12 +0900

    New Modules

--- ShiftWindowPackages/Package.swift
+++ ShiftWindowPackages/Package.swift
@@ -10,9 +10,22 @@ let package = Package(
     name: "ShiftWindowPackages",
     defaultLocalization: "en",
     platforms: [
-        .macOS(.v14)
+        .macOS(.v14),
     ],
     products: [
+        .library(
+            name: "Infrastructure",
+            targets: ["Infrastructure"]
+        ),
+        .library(
+            name: "Model",
+            targets: ["Model"]
+        ),
+        .library(
+            name: "Presentation",
+            targets: ["Presentation"]
+        ),
+
         .library(
             name: "LegacyDataLayer",
             targets: ["LegacyDataLayer"]
@@ -33,6 +46,37 @@ let package = Package(
         .package(url: "https://github.com/sparkle-project/Sparkle.git", exact: "2.6.4"),
     ],
     targets: [
+        .target(
+            name: "Infrastructure",
+            dependencies: [
+                .product(name: "Logging", package: "swift-log"),
+                .product(name: "Sparkle", package: "Sparkle"),
+                .product(name: "SpiceKey", package: "SpiceKey"),
+                .product(name: "WindowSceneKit", package: "WindowSceneKit"),
+            ],
+            swiftSettings: swiftSettings
+        ),
+        .target(
+            name: "Model",
+            dependencies: [
+                "Infrastructure",
+                .product(name: "Logging", package: "swift-log"),
+                .product(name: "SpiceKey", package: "SpiceKey"),
+            ],
+            swiftSettings: swiftSettings
+        ),
+        .target(
+            name: "Presentation",
+            dependencies: [
+                "Infrastructure",
+                "Model",
+                .product(name: "SpiceKey", package: "SpiceKey"),
+                .product(name: "WindowSceneKit", package: "WindowSceneKit"),
+            ],
+            swiftSettings: swiftSettings
+        ),
+
+
         .target(
             name: "LegacyDataLayer",
             dependencies: [
